4e9333c7f548808d5b7eec1d8e7d19475079af11
YuOkamoto <yuokamoto1988@gmail.com>
2022-12-01 11:45:48 +0900

    more skip attempts

--- BuildROS2/build_ros2_base.sh
+++ BuildROS2/build_ros2_base.sh
@@ -39,9 +39,9 @@ colcon build \
     --no-warn-unused-cli \
     --packages-skip \
         ros1_bridge \
-        example_interfaces \
         demo_nodes_cpp intra_process_demo quality_of_service_demo_cpp\
         rclc_examples \
+        example_interfaces \
         examples_rclcpp_minimal_action_client \
         examples_rclcpp_minimal_action_server \
         examples_rclcpp_minimal_client examples_rclcpp_minimal_service \
@@ -51,8 +51,8 @@ colcon build \
         examples_rclpy_minimal_publisher examples_rclpy_minimal_subscriber \
         examples_rclpy_minimal_client examples_rclpy_minimal_service \
         examples_rclpy_executors action_tutorials_py demo_nodes_py \
-        pendulum_control bag_recorder_nodes composition \
-        turtlesim \
+        orocos_kdl tf2_eigen_kdl \
+        cyclonedds rmw_cyclonedds_cpp\
         rviz_common rviz_ogre_vendor rviz_rendering rviz_rendering_tests \
         qt_gui_cpp qt_gui_core rqt_py_common rqt_gui \
         rclcpp \
@@ -62,20 +62,51 @@ colcon build \
         rosbag2_test_common rclcpp_action rclcpp_components\
         rclpy \
         rosidl_runtime_py quality_of_service_demo_py tf2_py rqt_gui_py\
-        ros2cli urdf\
+        ros2cli urdf urdfdom kdl_parser\
         launch_ros launch_testing_ros ros2launch tracetools_launch\
         ros2lifecycle_test_fixtures ros2multicast ros2run ros2trace rosbag2_storage  sros2_cmake test_communication test_security topic_monitor  \
-        action_tutorials_cpp demo_nodes_cpp_native examples_rclcpp_minimal_composition image_tools logging_demo ros2param ros2test rosbag2_cpp rosbag2_storage_default_plugins rqt rqt_console rqt_graph rqt_plot rqt_publisher rqt_py_console rqt_service_caller rqt_shell rqt_top rqt_topic test_launch_ros test_rclcpp tf2_ros tracetools_test\
-        examples_tf2_py robot_state_publisher ros2component ros_testing rosbag2_compression rosbag2_converter_default_plugins rqt_msg rqt_reconfigure tf2_bullet tf2_eigen tf2_geometry_msgs tf2_kdl tf2_sensor_msgs tf2_tools 
+        image_tools logging_demo ros2param ros2test rosbag2_cpp rosbag2_storage_default_plugins rqt rqt_console rqt_graph rqt_plot rqt_publisher rqt_py_console rqt_service_caller rqt_shell rqt_top rqt_topic test_launch_ros test_rclcpp tf2_ros tracetools_test\
+        robot_state_publisher ros2component ros_testing rosbag2_compression rosbag2_converter_default_plugins rqt_msg rqt_reconfigure tf2_bullet tf2_eigen tf2_geometry_msgs tf2_kdl tf2_sensor_msgs tf2_tools \
+        test_msgs sensor_msgs vidualization_msgs
 
         # geometry2 interactive_markers kdl_parser lifecycle rclc ros2action ros2doctor ros2interface ros2node ros2pkg ros2service ros2topic rosbag2_transport rqt_action rqt_srv rviz_visual_testing_framework sros2 test_tf2
-
         # rmw_connext_cpp rmw_connext_shared_cpp \
         # rosidl_typesupport_connext_c rosidl_typesupport_connext_cpp  \
-        # foonathan_memory_vendor mimick_vendor 
         # rclcpp \
         # cyclonedds \
         # rmw_cyclonedds_cpp \
         # rmw_fastrtps_cpp \
         # rmw_fastrtps_shared_cpp \
         # rosidl_runtime_cpp \
+
+
+        # demo_nodes_cpp intra_process_demo quality_of_service_demo_cpp\
+        # rclc_examples \
+        # example_interfaces \
+        # examples_rclcpp_minimal_action_client \
+        # examples_rclcpp_minimal_action_server \
+        # examples_rclcpp_minimal_client examples_rclcpp_minimal_service \
+        # examples_rclcpp_minimal_subscriber examples_rclcpp_minimal_publisher \
+        # examples_rclcpp_multithreaded_executor examples_rclcpp_minimal_timer \
+        # examples_rclpy_minimal_action_client examples_rclpy_minimal_action_server \
+        # examples_rclpy_minimal_publisher examples_rclpy_minimal_subscriber \
+        # examples_rclpy_minimal_client examples_rclpy_minimal_service \
+        # examples_rclpy_executors action_tutorials_py demo_nodes_py \
+
+    # --packages-skip-regex \
+    #     .*example.* \
+        # .*demo.* \
+        # .*tutorials.* \
+        # .*rviz.* \
+        # .*urdf.* \
+        # .*rqt.* \
+        # .*qt_gui.* \
+        # .*kdl.* \
+
+        # .*msgs.* \
+        # .*rclpy.* \
+        # .*rclcpp.* \
+        # .*msgs.* \
+        # .*tf2.* \
+        # .*launch.* \
+        # .*yaml.* \
